45e895a1bba9a33907bc76646872bec0a2eb9235
DmytroPopko <popkomu@gmail.com>
2022-10-19 18:56:23 +0300

    add task

--- src/components/Feedback/Statistics/Statistics.js
+++ src/components/Feedback/Statistics/Statistics.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Value from '../Value';
 
 const Statistics = ({
-  value: { good, neutral, bad, total, positivePercentage },
+  value: { good, neutral, bad, totalFeedback, positivePercentage },
 }) => (
   <div>
     <Value valueName={'Good'} value={good} />
     <Value valueName={'Neutral'} value={neutral} />
     <Value valueName={'Bad'} value={bad} />
-    <Value valueName={'Total'} value={total} />
+    <Value valueName={'Total'} value={totalFeedback} />
     <Value
       valueName={'Positive feedback'}
       value={isNaN(positivePercentage) ? 0 : positivePercentage}
@@ -18,11 +18,13 @@ const Statistics = ({
 );
 
 Statistics.propTypes = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
-  total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired,
+  value: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    totalFeedback: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Statistics;
